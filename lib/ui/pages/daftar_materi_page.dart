7ec3e47875c60c2a6a48cd4bc5c76ec870ee5961
ari efendi <ariefendi992@gmail.com>
2024-10-23 15:24:02 +0900

    update landscape daftar materi

--- lib/ui/pages/daftar_materi_page.dart
+++ lib/ui/pages/daftar_materi_page.dart
@@ -10,6 +10,131 @@ class DaftarMateriPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    Widget content() {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (screenWidth > 600) SizedBox(height: 32),
+            Text(
+              'DAFTAR MATERI',
+              style: blackTextStyle.copyWith(
+                fontWeight: bold,
+                fontSize: 32,
+                color: kBlackColor2,
+                fontFamily: caveatBrush,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: screenWidth > 600 ? 60 : defaultPadding),
+              margin: EdgeInsets.only(top: screenWidth > 600 ? 32 : 64),
+              child: Column(
+                children: [
+                  WidgetDaftarMateri(
+                    title: 'Pendahuluan',
+                    nomorHalaman: '3',
+                    onTap: () {
+                      Navigator.pushNamed(context, '/pendahuluan');
+                    },
+                  ),
+                  WidgetDaftarMateri(
+                    margin: EdgeInsets.only(top: 12),
+                    title: 'A. Karbohidrat',
+                    nomorHalaman: '4',
+                    onTap: () {
+                      Navigator.pushNamed(context, '/karbohidrat');
+                    },
+                  ),
+                  WidgetDaftarMateri(
+                    margin: EdgeInsets.only(top: 12),
+                    title: 'B. Asam Amino & Protein',
+                    nomorHalaman: '15',
+                    onTap: () {
+                      Navigator.pushNamed(context, '/asamAmino');
+                    },
+                  ),
+                  WidgetDaftarMateri(
+                    margin: EdgeInsets.only(top: 12),
+                    title: 'C. Asam Nukleat',
+                    nomorHalaman: '23',
+                    onTap: () {
+                      Navigator.pushNamed(context, '/asamNukleat');
+                    },
+                  ),
+                  WidgetDaftarMateri(
+                    margin: EdgeInsets.only(top: 12),
+                    title: 'D. Lipida',
+                    nomorHalaman: '28',
+                    onTap: () {
+                      Navigator.pushNamed(context, '/lipida');
+                    },
+                  ),
+                ],
+              ),
+            ),
+            screenWidth > 600
+                ? Container(
+                    margin: EdgeInsets.only(top: 44),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: defaultPadding,
+                      vertical: 20,
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: 350,
+                    // color: kRedColor1,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 180,
+                          child: CImagetAsset(
+                            imageName: 'assets/images/gambar1.crop.png',
+                            width: 290,
+                          ),
+                        ),
+                        Positioned(
+                          right: 200,
+                          bottom: 0,
+                          child: CImagetAsset(
+                            imageName: 'assets/images/gambar2.crop.png',
+                            width: 170,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                : Container(
+                    margin: EdgeInsets.only(top: 44),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: defaultPadding,
+                      vertical: 20,
+                    ),
+                    width: double.infinity,
+                    // height: 300,
+                    child: Stack(
+                      children: [
+                        CImagetAsset(
+                          imageName: 'assets/images/gambar1.crop.png',
+                          width: 240,
+                        ),
+                        Positioned(
+                          right: 0,
+                          bottom: 0,
+                          child: CImagetAsset(
+                            imageName: 'assets/images/gambar2.crop.png',
+                            width: 128,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+          ],
+        ),
+      );
+    }
+
     return PopScope(
       canPop: false,
       onPopInvokedWithResult: (didPop, result) {
@@ -106,121 +231,12 @@ class DaftarMateriPage extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'DAFTAR MATERI',
-                    style: blackTextStyle.copyWith(
-                      fontWeight: bold,
-                      fontSize: 32,
-                      color: kBlackColor2,
-                      fontFamily: caveatBrush,
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: defaultPadding),
-                    margin: EdgeInsets.only(top: 68),
-                    child: Column(
-                      children: [
-                        WidgetDaftarMateri(
-                          title: 'Pendahuluan',
-                          nomorHalaman: '3',
-                          onTap: () {
-                            Navigator.pushNamed(context, '/pendahuluan');
-                          },
-                        ),
-                        WidgetDaftarMateri(
-                          margin: EdgeInsets.only(top: 12),
-                          title: 'A. Karbohidrat',
-                          nomorHalaman: '4',
-                          onTap: () {
-                            Navigator.pushNamed(context, '/karbohidrat');
-                          },
-                        ),
-                        WidgetDaftarMateri(
-                          margin: EdgeInsets.only(top: 12),
-                          title: 'B. Asam Amino & Protein',
-                          nomorHalaman: '15',
-                          onTap: () {
-                            Navigator.pushNamed(context, '/asamAmino');
-                          },
-                        ),
-                        WidgetDaftarMateri(
-                          margin: EdgeInsets.only(top: 12),
-                          title: 'C. Asam Nukleat',
-                          nomorHalaman: '23',
-                          onTap: () {
-                            Navigator.pushNamed(context, '/asamNukleat');
-                          },
-                        ),
-                        WidgetDaftarMateri(
-                          margin: EdgeInsets.only(top: 12),
-                          title: 'D. Lipida',
-                          nomorHalaman: '28',
-                          onTap: () {
-                            Navigator.pushNamed(context, '/lipida');
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 44),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: defaultPadding,
-                      vertical: 20,
-                    ),
-                    width: double.infinity,
-                    // height: 300,
-                    child: Stack(
-                      children: [
-                        CImagetAsset(
-                          imageName: 'assets/images/gambar1.crop.png',
-                          width: 240,
-                        ),
-                        Positioned(
-                          right: 0,
-                          bottom: 0,
-                          child: CImagetAsset(
-                            imageName: 'assets/images/gambar2.crop.png',
-                            width: 128,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  // WButtonNextOrBack(
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, '/pendahuluan');
-                  //   },
-                  // )
-                ],
-              ),
-            ),
-            // Positioned(
-            //   bottom: 0,
-            //   right: 0,
-            //   child: Container(
-            //     alignment: Alignment.center,
-            //     width: 44,
-            //     height: 48,
-            //     decoration: BoxDecoration(
-            //         color: kBlueColor1,
-            //         borderRadius:
-            //             BorderRadius.only(topLeft: Radius.circular(50))),
-            //     child: Text(
-            //       '  2',
-            //       style: blackTextStyle.copyWith(
-            //         color: kBlackColor2,
-            //         fontSize: 26,
-            //         fontWeight: semiBold,
-            //       ),
-            //     ),
-            //   ),
-            // )
-            WNomorHalaman(nomorHalaman: '2')
+            screenWidth > 600
+                ? SingleChildScrollView(
+                    child: content(),
+                  )
+                : content(),
+            WNomorHalaman(nomorHalaman: '2'),
           ],
         ),
       ),
